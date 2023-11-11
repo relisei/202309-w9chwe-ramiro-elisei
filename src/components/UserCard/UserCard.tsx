9f2f6c159f76523d26fe07727f067fd81c6a109a
ramiro elisei <ramiroelisei@gmail.com>
2023-11-11 22:42:12 +0100

    Add UserCard component

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,40 @@
+import { UserStructure } from "../../store/features/users/types";
+import UserCardStyled from "./UserCardStyled";
+
+interface UserCardProps {
+  user: UserStructure;
+}
+
+const UserCard = ({
+  user: { nickname, firstName, lastName, picture, age, aboutMe },
+}: UserCardProps): React.ReactElement => {
+  return (
+    <UserCardStyled className="user-card">
+      <h2 className="user-card__nickname">@{nickname}</h2>
+      <img
+        className="user-card__picture"
+        src={picture}
+        alt={nickname}
+        width="300"
+        height="300"
+      />
+      <div className="user-card__information">
+        <div className="user-card__header-information">
+          <span className="user-card__fullname">
+            {firstName} {lastName}
+          </span>
+        </div>
+        <span className="user-card__age">
+          <span className="user-card__property">Age: </span>
+          {age}
+        </span>
+        <span className="user-card__aboutme">
+          <span className="user-card__property">About Me: </span>
+          {aboutMe}
+        </span>
+      </div>
+    </UserCardStyled>
+  );
+};
+
+export default UserCard;
