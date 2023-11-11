9251cd745d6f6608b85367be9e2f76348646e288
ramiro elisei <ramiroelisei@gmail.com>
2023-11-11 15:43:02 +0100

    Add Header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Header from "../Header/Header";
 
 const App = (): React.ReactElement => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): React.ReactElement => {
+  return (
+    <HeaderStyled className="header">
+      <h1 className="header__title">Hub Adversary</h1>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
