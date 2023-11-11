106eaa6fe47378d204406f76d594b7e1aa612f3c
ramiro elisei <ramiroelisei@gmail.com>
2023-11-11 15:59:56 +0100

    Add main imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
+import "@fontsource/rajdhani/700.css";
+import "@fontsource-variable/chivo";
+import GlobalStyles from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { mainTheme } from "./styles/mainTheme";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={mainTheme}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
