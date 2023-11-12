afa71de7e7e8653d9e61539d021b4c4c767f5253
ramiro elisei <ramiroelisei@gmail.com>
2023-11-12 21:25:34 +0100

    Fix path api url

--- src/store/features/users/usersSlice.ts
+++ src/store/features/users/usersSlice.ts
@@ -10,8 +10,9 @@ export const getUsers = createAsyncThunk(
   "users/getUsers",
   async (_data, { dispatch }) => {
     const { loadUsers } = usersSlice.actions;
-    const baseURL = import.meta.env.VITE_API_URL;
-    const response = await axios(`${baseURL}/users`);
+    const response = await axios(
+      "https://api-hub-adversary.onrender.com/users",
+    );
     dispatch(loadUsers(response.data));
   },
 );
