b085d37fb5e5070581d952c1d707b3831b90c0ff
ramiro elisei <ramiroelisei@gmail.com>
2023-11-12 13:03:41 +0100

    Add pagination in App

---
+++ src/pages/UsersPage/UsersPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageStyled from "./UsersPageStyled";
+
+const UsersPage = (): React.ReactElement => {
+  return (
+    <PageStyled>
+      <h2 className="user-page__title">Friend or Foe?</h2>
+    </PageStyled>
+  );
+};
+
+export default UsersPage;
